5878c38722602043c346477a5c6f1929a9fc3958
Benedikt Moritz Maurer <benedikt.moritz.maurer@physik.hu-berlin.de>
2024-10-01 18:15:42 +0200

    Add doxygen documentation

---
+++ dev/cmake/ConfigDoxygen.cmake
@@ -0,0 +1,18 @@
+# Find Doxygen package
+find_package(Doxygen REQUIRED)
+
+# Set Doxygen input and output paths
+set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile)
+set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/DoxygenDocs)
+
+# Create a custom command to run Doxygen
+add_custom_command(
+    OUTPUT ${DOXYGEN_OUT}
+    COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_IN}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    COMMENT "Generating API documentation with Doxygen"
+    VERBATIM
+)
+
+# Create a custom target for the documentation
+add_custom_target(doc ALL DEPENDS ${DOXYGEN_OUT})
