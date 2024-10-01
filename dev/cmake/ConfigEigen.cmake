2f9e0665b79f94b17f3c2e541392c8c8e2c8e4b2
Benedikt Moritz Maurer <benedikt.moritz.maurer@physik.hu-berlin.de>
2024-10-01 18:23:02 +0200

    use local libraries instead of fetching and building them

--- dev/cmake/ConfigEigen.cmake
+++ dev/cmake/ConfigEigen.cmake
@@ -1,14 +1,16 @@
-include(FetchContent)
+# include(FetchContent)
 
-# Declare the Eigen dependency
-FetchContent_Declare(
-  eigen
-  GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
-  GIT_TAG 3.4.0 # Specify the Eigen version you want to use
-)
+# # Declare the Eigen dependency
+# FetchContent_Declare(
+#   eigen
+#   GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
+#   GIT_TAG 3.4.0 # Specify the Eigen version you want to use
+# )
 
-# Make the content available
-FetchContent_MakeAvailable(eigen)
+# # Make the content available
+# FetchContent_MakeAvailable(eigen)
 
-# Optionally, include Armadillo headers if needed explicitly
-include_directories(${ARMADILLO_INCLUDE_DIRS})
+# # Optionally, include Armadillo headers if needed explicitly
+# include_directories(${ARMADILLO_INCLUDE_DIRS})
+
+find_package(Eigen3 REQUIRED)
